c67bf7e79b06af423a98fe12ca8e463a1ded222b
rajeev <mail2rajeevshukla@gmail.com>
2015-06-23 21:19:55 +0530

    done changes.

--- src/com/demo/cockies/ChangeBackgroundColor.java
+++ src/com/demo/cockies/ChangeBackgroundColor.java
@@ -16,6 +16,8 @@ import javax.servlet.http.HttpServletResponse;
 public class ChangeBackgroundColor extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
+	
+	
     /**
      * @see HttpServlet#HttpServlet()
      */
